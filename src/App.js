447d9dd224eb72c0ed9dde88d119184a2ff51c1d
fidelman <andrei.fidelman@actum.cz>
2019-02-02 14:25:35 +0100

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react'
+import { NavLink, Route } from 'react-router-dom'
+
+import AdminPage from './components/routes/admin'
+import AuthPage from './components/routes/auth'
 
 class App extends Component {
   render() {
+    const activeStyle = { color: 'red' }
+
     return (
-      <div>
-        Hello
-      </div>
+      <Fragment>
+        <div>
+          <NavLink to="/admin" activeStyle={activeStyle}>Admin</NavLink>
+        </div>
+        <div>
+          <NavLink to="/auth/sign-in" activeStyle={activeStyle}>Sign In</NavLink>
+        </div>
+        <div>
+          <NavLink to="/auth/sign-up" activeStyle={activeStyle}>Sign Up</NavLink>
+        </div>
+        <div>
+          <Route path="/admin" component={AdminPage} />
+          <Route path="/auth" component={AuthPage} />
+        </div>
+      </Fragment>
     );
   }
 }
 
-export default App;
+export default App
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
+import { routerMiddleware } from 'connected-react-router'
 import reducer from './reducer'
+import history from '../history'
 
-const enhancer = applyMiddleware(thunk ,logger)
+const enhancer = applyMiddleware(thunk, routerMiddleware(history), logger)
 
 const store = createStore(reducer, enhancer)
 
